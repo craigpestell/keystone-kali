849ace364d6ca52ee64b8bbda1e7575cdce5c7e2
craigpestell <craigpestell@gmail.com>
2016-05-04 23:21:20 -0700

    sortable technology

--- models/Technology.js
+++ models/Technology.js
@@ -7,7 +7,7 @@ var Types = keystone.Field.Types;
  */
 
 var Technology = new keystone.List('Technology', {
-	autokey: { from: 'name', path: 'key', unique: true }
+	autokey: { from: 'name', path: 'key', unique: true }, sortable: true
 });
 Technology.add({
 	name: { type: String, required: true, default: '' },
