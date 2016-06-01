8fe738a019b8c960283b33ea12619bb339decc38
craigpestell <craigpestell@gmail.com>
2016-05-31 22:02:52 -0700

    initial zoom icon setup

--- public/js/plugins/storeLocator/jquery.storelocator.js
+++ public/js/plugins/storeLocator/jquery.storelocator.js
@@ -5,6 +5,60 @@
 ;(function ($, window, document, undefined) {
 	'use strict';
 
+	var ZoomControl = function (google, controlDiv, map) {
+
+		// Creating divs & styles for custom zoom control
+		controlDiv.style.padding = '5px';
+
+		// Set CSS for the control wrapper
+		var controlWrapper = document.createElement('div');
+		controlWrapper.style.backgroundColor = 'white';
+		controlWrapper.style.borderStyle = 'solid';
+		controlWrapper.style.borderColor = 'gray';
+		controlWrapper.style.borderWidth = '1px';
+		controlWrapper.style.cursor = 'pointer';
+		controlWrapper.style.textAlign = 'center';
+		controlWrapper.style.width = '32px';
+		controlWrapper.style.height = '64px';
+		controlDiv.appendChild(controlWrapper);
+
+		// Set CSS for the zoomIn
+		var zoomInButton = document.createElement('div');
+		zoomInButton.style.width = '32px';
+		zoomInButton.style.height = '32px';
+		/* Change this to be the .png image you want to use */
+		zoomInButton.style.backgroundImage = 'url("http://placehold.it/32/00ff00")';
+		controlWrapper.appendChild(zoomInButton);
+
+		// Set CSS for the zoomOut
+		var zoomOutButton = document.createElement('div');
+		zoomOutButton.style.width = '32px';
+		zoomOutButton.style.height = '32px';
+		/* Change this to be the .png image you want to use */
+		zoomOutButton.style.backgroundImage = 'url("http://placehold.it/32/0000ff")';
+		controlWrapper.appendChild(zoomOutButton);
+
+		// Setup the click event listener - zoomIn
+		google.maps.event.addDomListener(zoomInButton, 'click', function() {
+			map.setZoom(map.getZoom() + 1);
+		});
+
+		// Setup the click event listener - zoomOut
+		google.maps.event.addDomListener(zoomOutButton, 'click', function() {
+			map.setZoom(map.getZoom() - 1);
+		});
+
+	};
+	var customizeZoomIcons = function(map){
+		// Create the DIV to hold the control and call the ZoomControl() constructor
+		// passing in this DIV.
+		var zoomControlDiv = document.createElement('div');
+		var zoomControl = new ZoomControl(google, zoomControlDiv, map);
+
+		zoomControlDiv.index = 1;
+		map.controls[google.maps.ControlPosition.TOP_RIGHT].push(zoomControlDiv);
+	};
+	
 	var pluginName = 'storeLocator';
 
 	// Only allow for one instantiation of this script and make sure Google Maps API is included
@@ -12,6 +66,9 @@
 		return;
 	}
 
+	
+	
+	
 	// Variables used across multiple methods
 	var $this, listTemplate, infowindowTemplate, dataTypeRead, originalOrigin, originalData, originalZoom, dataRequest, searchInput, addressInput, olat, olng, storeNum, directionsDisplay, directionsService, prevSelectedMarkerBefore, prevSelectedMarkerAfter, firstRun;
 	var featuredset = [], locationset = [], normalset = [], markers = [];
@@ -2099,7 +2156,7 @@
 
 			// Create the map
 			var map = new google.maps.Map(document.getElementById(_this.settings.mapID), myOptions);
-
+			customizeZoomIcons(map);
 			// Re-center the map when the browser is re-sized
 			google.maps.event.addDomListener(window, 'resize', function() {
 				var center = map.getCenter();
