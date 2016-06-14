1b9605cd3ce83eacc2bfc54c8f9470dc31357a17
craigpestell <craigpestell@gmail.com>
2016-06-13 19:45:17 -0700

    7/5 columns

--- routes/views/product.js
+++ routes/views/product.js
@@ -14,12 +14,12 @@ exports = module.exports = function(req, res) {
 		.populate('technologies features mainCategory subCategory'))
 		.then(function (err, results, next) {
 
-			
+			/*
 			locals.product.specs = locals.product.specs.replace(new RegExp('col-xs-5', 'g'), 'col-xs-6');
 			locals.product.specs = locals.product.specs.replace(new RegExp('col-xs-7', 'g'), 'col-xs-6');
 			locals.product.usageChart = locals.product.usageChart.replace(new RegExp('col-xs-5', 'g'), 'col-xs-6');
 			locals.product.usageChart = locals.product.usageChart.replace(new RegExp('col-xs-7', 'g'), 'col-xs-6');
-		
+			*/
 
 			locals.page = results.mainCategory.key;
 		locals.subCategory = results.subCategory.key;
