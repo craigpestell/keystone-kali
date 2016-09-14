36a356cb216751d0d3822c73a19d6c899c6a8804
Craig Pstell <craigpestell@gmail.com>
2016-09-13 23:03:36 -0700

    move banner image to base page

--- models/Page.js
+++ models/Page.js
@@ -10,6 +10,7 @@ BasePage.add(
 		title: { type: String, required: true },
 		slug: { type: String, readonly: true },
 		heroImage: {type: Types.CloudinaryImage,  autoCleanup : true},
+		bannerImage: { type: Types.CloudinaryImage,  autoCleanup : true},
 		carousel: {type: Types.CloudinaryImages,  autoCleanup : true},
 		slides: { type: Types.Relationship, ref: 'Slide', many: true },
 		contentHtml: {type: Types.Html}
