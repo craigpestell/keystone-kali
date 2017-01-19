dd1eb5a3622d835689c9a192c2254375e529a6f3
Craig Pstell <craigpestell@gmail.com>
2017-01-18 17:29:28 -0800

    re-import dealers

--- models/Dealer.js
+++ models/Dealer.js
@@ -16,7 +16,7 @@ Dealer.add({
 	email: {type: Types.Email}
 });
 
-/*Dealer.schema.pre('save', function(next) {
+Dealer.schema.pre('save', function(next) {
 	var self = this;
 	setTimeout(function(){
 		
@@ -31,7 +31,7 @@ Dealer.add({
 		});		
 	}, 1000);
 
- });*/
+ });
 /*
  "id": "9",
  "name": "Chipotle Minnetonka",
