c1f6ed62e1c82c81f09b0ea379ba0f03f3ca6728
Craig Pstell <craigpestell@gmail.com>
2017-05-31 18:20:46 -0700

    initial commit - gather posts on product-detail

--- routes/views/product.js
+++ routes/views/product.js
@@ -1,4 +1,7 @@
 var keystone = require('keystone');
+var async = require('async');
+
+var populatePost = require('../populate-post');
 
 exports = module.exports = function (req, res) {
 	
@@ -10,7 +13,44 @@ exports = module.exports = function (req, res) {
 
 	// Set locals
 	locals.section = 'product';
-	console.log('product slug:', req.params.product);
+
+
+
+	// Load the posts
+	var loadPosts = function(productId, next){
+
+		keystone.list('categories').model.findOne()
+		var q = keystone.list('Post')
+			.where('state', 'published')
+			.sort('-publishedDate')
+			.populate('author categories product postLayout gallery.widgets product product');
+		q.where('categories').in([locals.data.category])//.findOne({product:productId});
+		
+
+		q.exec(function (err, results) {
+			//console.log('here:', results);
+			locals.data.posts = results;
+			if(results){
+				async.forEachOf(results.results, function (post, i, cb) {
+	
+						populatePost(post, cb);
+						locals.data.posts[i] = post;
+						
+					},
+					function (err) {
+						if (err) {
+							console.log('error', err);
+						}
+						console.log('done async 1');
+						next(err);
+					}
+				);
+			}else{
+				next();
+			}
+		});
+	};
+
 	view.query('product', keystone.list('Product').model.findOne({slug:req.params.product})
 		.populate('canonicalDiscipline technologies features mainCategory subCategory sizingChart'))
 		.then(function (err, results, next) {
@@ -28,7 +68,8 @@ exports = module.exports = function (req, res) {
 				locals.data.page.title = results.name + ' - Kali Protectives';
 			
 				if (err) return next(err);
-				next();
+				
+				loadPosts(results._id, next);
 			}
 		});
 	
