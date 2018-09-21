20ab94d3565ac586203bb14430e356709ce6b737
Craig Pestell <craigpestell@gmail.com>
2018-09-21 13:10:56 -0700

    fix ProductTechnology sorting on tech page, add defaultColumns and sorting to admin

--- models/ProductTechnology.js
+++ models/ProductTechnology.js
@@ -1,4 +1,4 @@
-var keystone = require('keystone');
+var keystone = require("keystone");
 var Types = keystone.Field.Types;
 
 /**
@@ -6,28 +6,35 @@ var Types = keystone.Field.Types;
  * =============
  */
 
-var ProductTechnology = new keystone.List('ProductTechnology', {
-	autokey: { from: 'name', path: 'key', unique: true }, sortable: true
+var ProductTechnology = new keystone.List("ProductTechnology", {
+	autokey: { from: "name", path: "key", unique: true },
+	sortable: true
 });
-ProductTechnology.relationship({ path: 'products', ref: 'Product', refPath: 'technologies' });
+ProductTechnology.relationship({
+	path: "products",
+	ref: "Product",
+	refPath: "technologies"
+});
+ProductTechnology.defaultColumns = "year, name";
+ProductTechnology.defaultSort = "year";
 ProductTechnology.add({
-	name: { type: String, required: true, default: '' },
+	name: { type: String, required: true, default: "" },
 	slug: { type: String },
-	shortDescription: { type: Types.Html, default: '' },
-	description: { type: Types.Html, default: '' },
-	year:{ type: Types.Number },
-	iconImage: { type: Types.CloudinaryImage,  autoCleanup : true},
-	flatIconImage: { type: Types.CloudinaryImage,  autoCleanup : true},
-	rawIconImage: { type: String},
-	heroImage: { type: Types.CloudinaryImage,  autoCleanup : true},
-	heroImageMoto: { type: Types.CloudinaryImage,  autoCleanup : true},
-	bannerImage: { type: Types.CloudinaryImage,  autoCleanup : true},
-	bannerOverlayHtml: { type: Types.Html, default: '' },
-	primaryImage:  { type: Types.CloudinaryImage,  autoCleanup : true},
-	secondaryImage: { type: Types.CloudinaryImage,  autoCleanup : true},
-	tertiaryImage: { type: Types.CloudinaryImage,  autoCleanup : true},
-	contentHtml: { type: Types.Html, default: '' },
-	post:{ type: Types.Relationship, ref: 'Post'}
+	shortDescription: { type: Types.Html, default: "" },
+	description: { type: Types.Html, default: "" },
+	year: { type: Types.Number },
+	iconImage: { type: Types.CloudinaryImage, autoCleanup: true },
+	flatIconImage: { type: Types.CloudinaryImage, autoCleanup: true },
+	rawIconImage: { type: String },
+	heroImage: { type: Types.CloudinaryImage, autoCleanup: true },
+	heroImageMoto: { type: Types.CloudinaryImage, autoCleanup: true },
+	bannerImage: { type: Types.CloudinaryImage, autoCleanup: true },
+	bannerOverlayHtml: { type: Types.Html, default: "" },
+	primaryImage: { type: Types.CloudinaryImage, autoCleanup: true },
+	secondaryImage: { type: Types.CloudinaryImage, autoCleanup: true },
+	tertiaryImage: { type: Types.CloudinaryImage, autoCleanup: true },
+	contentHtml: { type: Types.Html, default: "" },
+	post: { type: Types.Relationship, ref: "Post" }
 });
 
 ProductTechnology.register();
--- routes/views/technology.js
+++ routes/views/technology.js
@@ -1,179 +1,166 @@
-var keystone = require('keystone');
-var Post = keystone.list('Post');
-var Product = keystone.list('Product');
+var keystone = require("keystone");
+var Post = keystone.list("Post");
+var Product = keystone.list("Product");
 //var PostComment = keystone.list('PostComment');
-var async = require('async');
-var populatePost = require('../populate-post');
+var async = require("async");
+var populatePost = require("../populate-post");
 
-exports = module.exports = function (req, res) {
-
+exports = module.exports = function(req, res) {
 	var view = new keystone.View(req, res);
 	var locals = res.locals;
 	locals.data = {};
 	console.log(req.params);
 	// Init locals
-	locals.section = 'technology';
+	locals.section = "technology";
 	locals.filters = {
-		key: 'technology',
+		key: "technology"
 	};
 
 	//console.log(req.originalUrl);
 
 	// Load the current post
-	view.on('init', function (next) {
+	view.on("init", function(next) {
+		var q = Post.model
+			.findOne({
+				//state: 'published',
+				key: locals.filters.key
+			})
+			.populate("author categories product gallery.widgets");
 
-		var q = Post.model.findOne({
-			//state: 'published',
-			key: locals.filters.key
-		}).populate('author categories product gallery.widgets');
+		q.exec(function(err, result) {
+			if (result) {
+				if (result.product) {
+					var q = Product.model
+						.findOne({ _id: result.product })
+						.populate("technologies mainTechnology subTechnology");
 
-		q.exec(function (err, result) {
-
-			if(result){
-				if(result.product){
-					var  q = Product.model.findOne({_id: result.product}).populate('technologies mainTechnology subTechnology');
-
-					q.exec(function(err, product){
+					q.exec(function(err, product) {
 						locals.product = product;
-						populatePost(result, function(){
+						populatePost(result, function() {
 							locals.post = result;
 							next(err);
 						});
-					})
-				}else{
-					populatePost(result, function(){
+					});
+				} else {
+					populatePost(result, function() {
 						locals.post = result;
 						next(err);
 					});
 				}
-			}else{
+			} else {
 				next(err);
 			}
-
 		});
 	});
 
 	// Load products
-	view.on('init', function (next) {
-
+	view.on("init", function(next) {
 		var q = Post.model.findOne({
 			//state: 'published',
 			key: locals.filters.key
-		});//.populate('products');
+		}); //.populate('products');
 
-		q.exec(function (err, result) {
-
-			if(result){
-				populatePost(result, function(){
+		q.exec(function(err, result) {
+			if (result) {
+				populatePost(result, function() {
 					//locals.post = result;
 					next(err);
 				});
-			}else{
+			} else {
 				next(err);
 			}
-
-
 		});
 	});
 
 	// Load technologies
-	view.on('init', function (next) {
+	view.on("init", function(next) {
+		var q = keystone.list("Technology").model.find();
 
-		var q = keystone.list('Technology').model.find();
-
-		q.exec(function (err, result) {
-
-			if(result){
+		q.exec(function(err, result) {
+			if (result) {
 				locals.technology = result;
 				next(err);
-			}else{
+			} else {
 				next(err);
 			}
-
-
 		});
 	});
 
 	// Load technologies
-	view.on('init', function (next) {
+	view.on("init", function(next) {
+		var q = keystone
+			.list("ProductTechnology")
+			.model.find()
+			.sort("year");
 
-		var q = keystone.list('ProductTechnology').model.find();
-
-		q.exec(function (err, result) {
-
-			if(result){
+		q.exec(function(err, result) {
+			if (result) {
 				locals.productTechnology = result;
 				next(err);
-			}else{
+			} else {
 				next(err);
 			}
-
-
 		});
 	});
 
-// Load the posts
-	view.on('init', function (next) {
-
+	// Load the posts
+	view.on("init", function(next) {
 		/*var q = keystone.list('Post').paginate({
 			page: req.query.page || 1,
 			perPage: 10,
 			maxPages: 10
 		})*/
-		if(process.env.DO) {
-			locals.data.category = '5a1a5061bc13d294547da833';
-		}else{
-			locals.data.category = '5a1a28f4ecddff59637a740c';
+		if (process.env.DO) {
+			locals.data.category = "5a1a5061bc13d294547da833";
+		} else {
+			locals.data.category = "5a1a28f4ecddff59637a740c";
 		}
 
-		var filters = {'state': 'published'};
+		var filters = { state: "published" };
 		if (locals.data.category) {
-			filters.categories = {$in: [locals.data.category]};
+			filters.categories = { $in: [locals.data.category] };
 		}
 
+		var q = keystone
+			.list("Post")
+			.paginate({
+				page: req.query.page || 1,
+				perPage: 20,
+				maxPages: 10,
+				filters: filters
+			})
+			.where("state", "published")
+			.sort("-publishedDate")
+			.populate("author categories product postLayout gallery.widgets");
 
-		var q = keystone.list('Post').paginate({
-			page: req.query.page || 1,
-			perPage: 20,
-			maxPages: 10,
-			filters: filters
-		})
-			.where('state', 'published')
-			.sort('-publishedDate')
-			.populate('author categories product postLayout gallery.widgets');
-		
 		if (locals.data.category) {
-			q.where('categories').in([locals.data.category]);
+			q.where("categories").in([locals.data.category]);
 		}
 
-		q.exec(function (err, results) {
+		q.exec(function(err, results) {
 			//console.log('here:', results);
 			locals.data.posts = results.results;
-			if(!results){
+			if (!results) {
 				next(err);
 				return;
 			}
-			async.forEachOf(results, function (post, i, cb) {
-
+			async.forEachOf(
+				results,
+				function(post, i, cb) {
 					populatePost(post, cb);
 					locals.data.posts[i] = post;
-
 				},
-				function (err) {
+				function(err) {
 					if (err) {
-						console.log('error', err);
+						console.log("error", err);
 					}
-					
+
 					next(err);
 				}
 			);
-
 		});
-
 	});
 
-
 	// Render the view
-	view.render('technology');
-
-}
+	view.render("technology");
+};
