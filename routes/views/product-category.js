b3b203510642b1e79342cd3e27e37051145e01b2
Craig Pestell <craigpestell@gmail.com>
2018-09-30 17:29:32 -0700

    adding code for canonical product category pages

--- routes/views/product-category.js
+++ routes/views/product-category.js
@@ -5,115 +5,133 @@ var async = require('async');
  * Get all subcategories and products in main category
  * */
 var productCategoryData = function(locals, productDataCb) {
-    var disciplineWhere = { slug: '' };
-    if (locals.params.discipline) {
-        disciplineWhere.slug = locals.params.discipline.slug;
-    }
+	var disciplineWhere = { slug: '' };
+	if (locals.params.discipline) {
+		disciplineWhere.slug = locals.params.discipline.slug;
+	}
 
-    var versionWhere = { version: locals.currentProductVersion._id };
-    if (locals.params.version) {
-        locals.productVersions.forEach(function(version) {
-                if (version.key === locals.params.version.key) {
-                    versionWhere = { version: version._id }
-                }
-            })
-            //versionWhere = params.version;
-    }
-    var categoryWhere = {};
-    if (locals.params.category) {
-        categoryWhere = { slug: locals.params.category.slug };
-    }
-    var subCategoryWhere = {};
-    if (locals.params.subCategory) {
-        subCategoryWhere = { slug: locals.params.subCategory.slug };
-    }
+	var versionWhere = { version: locals.currentProductVersion._id };
+	if (locals.params.version) {
+		locals.productVersions.forEach(function(version) {
+			if (version.key === locals.params.version.key) {
+				versionWhere = { version: version._id };
+			}
+		});
+		//versionWhere = params.version;
+	}
+	var categoryWhere = {};
+	if (locals.params.category) {
+		categoryWhere = { slug: locals.params.category.slug };
+	}
+	var subCategoryWhere = {};
+	if (locals.params.subCategory) {
+		subCategoryWhere = { slug: locals.params.subCategory.slug };
+	}
 
-    keystone.list('Discipline').model.find().where(disciplineWhere).exec(function(err, discipline) {
-        var disciplineWhere = {};
-        if (discipline.length > 0) {
-            disciplineWhere = { disciplines: discipline[0]._id };
-        }
-        async.parallel({
-                categories: function(callback) {
-                    keystone.list('ProductCategory')
-                        .model.find(categoryWhere).exec(callback);
-                },
-                subcategories: function(callback) {
-                    keystone.list('ProductSubCategory')
-                        .model.find(subCategoryWhere).where(disciplineWhere).sort('sortOrder').exec(callback);
-                },
-                products: function(callback) {
-                    keystone.list('Product')
-                        .model.find().where(Object.assign(disciplineWhere, versionWhere))
-                        .populate('mainCategory subCategory technologies').sort('sortOrder').exec(callback);
-                }
-            },
-            function massage(err, results) {
-                var returnData = [];
-                //console.log('results:', results);
-                results.categories.forEach(function(cat) {
-                    results.subcategories.forEach(function(subCat, j) {
-                        if (subCat.products === undefined) {
-                            subCat.products = [];
-                        }
-                        //get all products for sub category
-                        results.products.forEach(function(product) {
-                            if (subCat._id.equals(product.subCategory.id) && cat._id.equals(subCat.parentCategory)) {
-                                // Find correct version:
-                                // if there is no version param find the most recent version
-                                subCat.products.push(product);
-                            }
-                        });
+	keystone
+		.list('Discipline')
+		.model.find()
+		.where(disciplineWhere)
+		.exec(function(err, discipline) {
+			var disciplineWhere = {};
+			if (discipline.length > 0) {
+				disciplineWhere = { disciplines: discipline[0]._id };
+			}
+			async.parallel(
+				{
+					categories: function(callback) {
+						keystone
+							.list('ProductCategory')
+							.model.find(categoryWhere)
+							.exec(callback);
+					},
+					subcategories: function(callback) {
+						keystone
+							.list('ProductSubCategory')
+							.model.find(subCategoryWhere)
+							.where(disciplineWhere)
+							.sort('sortOrder')
+							.exec(callback);
+					},
+					products: function(callback) {
+						keystone
+							.list('Product')
+							.model.find()
+							.where(Object.assign(disciplineWhere, versionWhere))
+							.populate('mainCategory subCategory technologies')
+							.sort('sortOrder')
+							.exec(callback);
+					},
+				},
+				function massage(err, results) {
+					var returnData = [];
+					//console.log('results:', results);
+					results.categories.forEach(function(cat) {
+						results.subcategories.forEach(function(subCat, j) {
+							if (subCat.products === undefined) {
+								subCat.products = [];
+							}
+							//get all products for sub category
+							results.products.forEach(function(product) {
+								if (subCat._id.equals(product.subCategory.id) && cat._id.equals(subCat.parentCategory)) {
+									// Find correct version:
+									// if there is no version param find the most recent version
+									subCat.products.push(product);
+								}
+							});
 
-                        if (subCat.products.length > 0 && cat._id.equals(subCat.parentCategory)) {
-                            returnData.push(subCat);
-                        }
-                    });
-                });
-                results.subcategories = results.subcategories.filter(function(subCategory) {
-                    return subCategory.products.length > 0;
-                });
-                //console.log("MASSAGED RESULTS: ", returnData);
-                //console.log(returnData.categories.bike.subCategories);
-                productDataCb(null, returnData);
-            }
-        );
-    });
+							if (subCat.products.length > 0 && cat._id.equals(subCat.parentCategory)) {
+								returnData.push(subCat);
+							}
+						});
+					});
+					results.subcategories = results.subcategories.filter(function(subCategory) {
+						return subCategory.products.length > 0;
+					});
+					//console.log("MASSAGED RESULTS: ", returnData);
+					//console.log(returnData.categories.bike.subCategories);
+					productDataCb(null, returnData);
+				},
+			);
+		});
 };
 
-
-
 exports = module.exports = function(req, res, next) {
-    var view = new keystone.View(req, res);
-    var locals = res.locals;
-    // Set locals
-    locals.page = req.params.category;
+	var view = new keystone.View(req, res);
+	var locals = res.locals;
+	// Set locals
+	locals.page = req.params.category;
 
-    locals.data = {
-        page: { title: 'Kali Protectives' }
-    };
+	locals.data = {
+		page: { title: 'Kali Protectives' },
+	};
 
-    locals.section = 'products';
+	locals.section = 'products';
 
-    if (res.locals.params.category || res.locals.params.version) {
-        //console.log('params:', res.locals.params);
-        productCategoryData(res.locals, function(err, productCategoryData) {
+	if (res.locals.params.category || res.locals.params.version) {
+		//console.log('params:', res.locals.params);
+		productCategoryData(res.locals, function(err, productCategoryData) {
+			locals.products = productCategoryData;
+			//console.log('products:',locals.products);
 
-            locals.products = productCategoryData;
-            //console.log('products:',locals.products);
-
-            //console.log('category param: ', res.locals.params.category);
-            var titleCategory = 'Archives';
-            if (res.locals.params.category) {
-                titleCategory = res.locals.params.category.name;
-            }
-            locals.data.page.title = titleCategory + ' - ' + locals.data.page.title;
-            if (res.locals.params.subCategory) {
-                locals.data.page.title = res.locals.params.subCategory.name + ' - ' + locals.data.page.title;
-            }
-            view.render('product-category');
-        });
-    } else {
-        next();
-    }
+			//console.log('category param: ', res.locals.params.category);
+			if (res.locals.params.discipline) {
+				locals.data.discipline = res.locals.params.discipline.slug;
+			}
+			if (res.locals.params.category) {
+				locals.data.category = res.locals.params.category.slug;
+			}
+			var titleCategory = 'Archives';
+			if (res.locals.params.category) {
+				titleCategory = res.locals.params.category.name;
+			}
+			locals.data.page.title = titleCategory + ' - ' + locals.data.page.title;
+			if (res.locals.params.subCategory) {
+				locals.data.page.title = res.locals.params.subCategory.name + ' - ' + locals.data.page.title;
+			}
+			view.render('product-category');
+		});
+	} else {
+		next();
+	}
 };
--- routes/views/product.js
+++ routes/views/product.js
@@ -3,104 +3,108 @@ var async = require('async');
 
 var populatePost = require('../populate-post');
 
-exports = module.exports = function(req, res) {
+exports = module.exports = function (req, res) {
 
-    var view = new keystone.View(req, res);
-    var locals = res.locals;
-    locals.data = {
-        page: []
-    };
+	var view = new keystone.View(req, res);
+	var locals = res.locals;
+	locals.data = {
+		page: []
+	};
 
-    // Set locals
-    locals.section = 'product';
-
-
-    var loadMainPost = function(product, next) {
-        var q = keystone.list('Post').model.findOne()
-            .where({ _id: { $in: [product.mainPost] } })
-            .sort('-publishedDate')
-            .populate('author categories product postLayout gallery.widgets');
-
-        q.exec(function(err, results) {
-            locals.data.mainPost = results;
-            next();
-        });
-    };
+	// Set locals
+	locals.section = 'product';
 
 
-    // Load the posts
-    var loadPosts = function(product, next) {
-        var q = keystone.list('Post').model
-            .where({ products: { $in: [product._id] }, state: 'published' })
-            .sort('-publishedDate')
-            .populate('author categories product postLayout gallery.widgets');
+	var loadMainPost = function (product, next) {
+		var q = keystone.list('Post').model.findOne()
+			.where({_id: {$in: [product.mainPost]}})
+			.sort('-publishedDate')
+			.populate('author categories product postLayout gallery.widgets');
 
-        q.exec(function(err, results) {
-            locals.data.posts = results;
-            next();
-        });
+		q.exec(function (err, results) {
+			locals.data.mainPost = results;
+			next();
+		});
+	};
 
-    };
-    var productWhere = { slug: req.params.product };
-    view.query('product', keystone.list('Product').model.find(productWhere)
-            .populate('canonicalDiscipline technologies features mainCategory subCategory sizingChart version'))
-        .then(function(err, products, next) {
-            if (!products) {
-                next();
-            } else {
 
-                if (err) return next(err);
+	// Load the posts
+	var loadPosts = function (product, next) {
+		var q = keystone.list('Post').model
+			.where({products: {$in: [product._id]}, state: 'published'})
+			.sort('-publishedDate')
+			.populate('author categories product postLayout gallery.widgets');
 
-                var p = undefined;
-                if (res.locals.params.version) {
-                    products.forEach(function(product) {
-                        if (res.locals.params.version.key === product.version.key) {
-                            locals.page = product.mainCategory.key;
-                            locals.subCategory = product.subCategory.key;
-                            locals.data.page.title = product.name + ' - Kali Protectives';
-                            if (!p) {
-                                p = product;
-                            }
-                        }
-                    });
-                } else {
-                    //find latest version
-                    console.log('finding latest..');
-                    var latestVersion = Math.max.apply(Math, res.locals.productVersions.map(function(v) { return parseInt(v.name) }))
-                    products.forEach(function(product) {
-			// if the version param was specified, just find that specific version.
-			// current version is never than stored.
-			if(!p || (product.version.key >p.version.key )){
-				p = product;
+		q.exec(function (err, results) {
+			locals.data.posts = results;
+			next();
+		});
+
+	};
+	var productWhere = {slug: req.params.product};
+	view.query('product', keystone.list('Product').model.find(productWhere)
+		.populate('canonicalDiscipline technologies features mainCategory subCategory sizingChart version'))
+		.then(function (err, products, next) {
+			if (!products) {
+				next();
+			} else {
+
+				if (err) return next(err);
+
+				var p = undefined;
+				if (res.locals.params.version) {
+					products.forEach(function (product) {
+						if (res.locals.params.version.key === product.version.key) {
+							locals.page = product.mainCategory.key;
+							locals.subCategory = product.subCategory.key;
+							locals.data.page.title = product.name + ' - Kali Protectives';
+							if (!p) {
+								p = product;
+							}
+						}
+					});
+				} else {
+					//find latest version
+					console.log('finding latest..');
+					var latestVersion = Math.max.apply(Math, res.locals.productVersions.map(function (v) {
+						return parseInt(v.name)
+					}))
+					products.forEach(function (product) {
+						// if the version param was specified, just find that specific version.
+						// current version is never than stored.
+						if (!p || (product.version.key > p.version.key)) {
+							p = product;
+						}
+						console.log('product.version: ', product.version);
+						if (latestVersion == product.version.key) {
+							locals.page = product.mainCategory.key;
+							locals.subCategory = product.subCategory.key;
+							locals.data.page.title = product.name + ' - Kali Protectives';
+							p = product;
+						}
+					});
+					console.log('end of loop, p: ', p);
+
+					// });
+				}
+				if (!p) {
+					res.redirect('/404');
+				}
+				console.log('p.version.key: ', p.version.key);
+				if (!res.locals.params.version && p.version.key != latestVersion) {
+					return res.redirect(301, '/achive/' + p.version.key + req.originalUrl);
+				}
+
+
+				locals.product = p;
+				loadPosts(p, function () {
+					loadMainPost(p, function () {
+						populatePost(p, next);
+					});
+				});
 			}
-			console.log('product.version: ', product.version);
-			if (latestVersion == product.version.key) {
-                            locals.page = product.mainCategory.key;
-                            locals.subCategory = product.subCategory.key;
-                            locals.data.page.title = product.name + ' - Kali Protectives';
-			    p = product;
-                        }
-                    });
-                    // });
-                }
-		console.log('p.version.key: ', p.version.key);
-                if(!res.locals.params.version && p.version.key != latestVersion){
-			return res.redirect(301, '/achive/' + p.version.key + req.originalUrl);
-		}
+		});
 
-
-                if (!p) {
-                    res.redirect('/404');
-                }
-                locals.product = p;
-                loadPosts(p, function() {
-                    loadMainPost(p, function() {
-                        populatePost(p, next);
-                    });
-                });
-            }
-        });
-
-    view.render('product');
+	view.render('product');
 
 };
