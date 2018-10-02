a2a7bd18e617a8869ea236dacb1746c98b01d502
Craig Pestell <craigpestell@gmail.com>
2018-10-01 20:04:03 -0700

    fix discipline in redirects

--- routes/views/product.js
+++ routes/views/product.js
@@ -3,57 +3,61 @@ var async = require('async');
 
 var populatePost = require('../populate-post');
 
-exports = module.exports = function (req, res) {
-
+exports = module.exports = function(req, res) {
 	var view = new keystone.View(req, res);
 	var locals = res.locals;
 	locals.data = {
-		page: []
+		page: [],
 	};
 
 	// Set locals
 	locals.section = 'product';
 
-
-	var loadMainPost = function (product, next) {
-		var q = keystone.list('Post').model.findOne()
-			.where({_id: {$in: [product.mainPost]}})
+	var loadMainPost = function(product, next) {
+		var q = keystone
+			.list('Post')
+			.model.findOne()
+			.where({ _id: { $in: [product.mainPost] } })
 			.sort('-publishedDate')
 			.populate('author categories product postLayout gallery.widgets');
 
-		q.exec(function (err, results) {
+		q.exec(function(err, results) {
 			locals.data.mainPost = results;
 			next();
 		});
 	};
 
-
 	// Load the posts
-	var loadPosts = function (product, next) {
-		var q = keystone.list('Post').model
-			.where({products: {$in: [product._id]}, state: 'published'})
+	var loadPosts = function(product, next) {
+		var q = keystone
+			.list('Post')
+			.model.where({ products: { $in: [product._id] }, state: 'published' })
 			.sort('-publishedDate')
 			.populate('author categories product postLayout gallery.widgets');
 
-		q.exec(function (err, results) {
+		q.exec(function(err, results) {
 			locals.data.posts = results;
 			next();
 		});
-
 	};
-	var productWhere = {slug: req.params.product};
-	view.query('product', keystone.list('Product').model.find(productWhere)
-		.populate('canonicalDiscipline technologies features mainCategory subCategory sizingChart version'))
-		.then(function (err, products, next) {
+	var productWhere = { slug: req.params.product };
+	view
+		.query(
+			'product',
+			keystone
+				.list('Product')
+				.model.find(productWhere)
+				.populate('canonicalDiscipline technologies features mainCategory subCategory sizingChart version'),
+		)
+		.then(function(err, products, next) {
 			if (!products) {
 				next();
 			} else {
-
 				if (err) return next(err);
 
 				var p = undefined;
 				if (res.locals.params.version) {
-					products.forEach(function (product) {
+					products.forEach(function(product) {
 						if (res.locals.params.version.key === product.version.key) {
 							locals.page = product.mainCategory.key;
 							locals.subCategory = product.subCategory.key;
@@ -66,13 +70,16 @@ exports = module.exports = function (req, res) {
 				} else {
 					//find latest version
 					console.log('finding latest..');
-					var latestVersion = Math.max.apply(Math, res.locals.productVersions.map(function (v) {
-						return parseInt(v.name)
-					}))
-					products.forEach(function (product) {
+					var latestVersion = Math.max.apply(
+						Math,
+						res.locals.productVersions.map(function(v) {
+							return parseInt(v.name);
+						}),
+					);
+					products.forEach(function(product) {
 						// if the version param was specified, just find that specific version.
 						// current version is never than stored.
-						if (!p || (product.version.key > p.version.key)) {
+						if (!p || product.version.key > p.version.key) {
 							p = product;
 						}
 						console.log('product.version: ', product.version);
@@ -92,13 +99,16 @@ exports = module.exports = function (req, res) {
 				}
 				console.log('p.version.key: ', p.version.key);
 				if (!res.locals.params.version && p.version.key != latestVersion) {
-					return res.redirect(301, '/archive/' + p.version.key + req.originalUrl);
+					return res.redirect(
+						301,
+						'https://' +
+							(p.canonicalDiscipline.slug + '.kaliprotectives.com/archive/' + p.version.key + req.originalUrl),
+					);
 				}
 
-
 				locals.product = p;
-				loadPosts(p, function () {
-					loadMainPost(p, function () {
+				loadPosts(p, function() {
+					loadMainPost(p, function() {
 						populatePost(p, next);
 					});
 				});
@@ -106,5 +116,4 @@ exports = module.exports = function (req, res) {
 		});
 
 	view.render('product');
-
 };
